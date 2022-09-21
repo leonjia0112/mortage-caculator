47bf8807de97333958aa604b1370491b9036e376
Pei Jia <leojia@bu.edu>
2022-09-20 21:28:50 -0400

    Improve mortgage calculation function

--- fix-rate-monthly-payment.py
+++ fix-rate-monthly-payment.py
@@ -5,58 +5,69 @@ import pprint
 month_detail_list = {}
 
 
-def mortgate_calculater(principal, apr, length, is_year=False):
+def calculate_mortage_monthly_payment(principal, monthly_rate, number_of_payment):
+    monthly_payment = ((principal * monthly_rate) \
+                       * math.pow(1 + monthly_rate, number_of_payment)) \
+                      / (math.pow(1 + monthly_rate, number_of_payment) - 1)
+    return monthly_payment
+
+
+def calculate_total_interest_predict(principal, number_of_payment, monthly_rate):
+    # This calculates the total interest pay with this loan
+    total_interest = (number_of_payment * principal * monthly_rate * \
+                      math.pow(1 + monthly_rate, number_of_payment)) \
+                     / (math.pow(1 + monthly_rate, number_of_payment) - 1) - principal
+
+
+def mortgate_calculater(price, down_payment_ammount, apr, length, is_year=False, extra_principal_paid: dict = {}):
     """
     :params: length: length of loan time
     :params: apr: fixed apr rate
     :params: principal: loan amount
     :params: is_year: default to False, if given length is year, set to True
     """
-    remain_principal = principal
-    monthly_rate = apr / 12
+    remain_principal = price - down_payment_ammount
+    print(f'Down Payment: {price - remain_principal}')
+    monthly_rate = (apr / 100) / 12
     number_of_payment = length * 12 if is_year else length
-
-    # This calculates the monthly payment
-    monthly_payment = ((principal * monthly_rate) \
-                       * math.pow(1 + monthly_rate, number_of_payment)) \
-                      / (math.pow(1 + monthly_rate, number_of_payment) - 1)
-
-    print("Monthly payment is {}".format(monthly_payment))
-
-    print("There are {} month, which are {} years to get to the loan amount.".format(
-        round(remain_principal / monthly_payment),
-        round(round(remain_principal / monthly_payment) / 12)))
-
-    # This calculates the total interest pay with this loan
-    total_interest = (number_of_payment * remain_principal * monthly_rate * \
-                      math.pow(1 + monthly_rate, number_of_payment)) \
-                     / (math.pow(1 + monthly_rate, number_of_payment) - 1) - remain_principal
-    print("Total interest paid: {}".format(total_interest))
 
     mortgate_details = []
     interest_pay_so_far = 0
+    the_nth_month = 1
 
-    for i in range(number_of_payment):
-        interest = remain_principal * monthly_rate
-        remain_principal = remain_principal - (monthly_payment - interest)
-        principal_paid = monthly_payment - interest
-        interest_pay_so_far = interest_pay_so_far + interest
+    while number_of_payment > 0:
+        # This calculates the monthly payment
+        monthly_payment = ((remain_principal * monthly_rate) \
+                           * math.pow(1 + monthly_rate, number_of_payment)) \
+                          / (math.pow(1 + monthly_rate, number_of_payment) - 1)
+
+        # print("Monthly payment is {}".format(monthly_payment))
+
+        interest_paid = remain_principal * monthly_rate
+        principal_paid = monthly_payment - interest_paid
+
+        remain_principal = remain_principal - principal_paid - extra_principal_paid.get(the_nth_month, 0)
+        principal_paid = monthly_payment - interest_paid
+        interest_pay_so_far = interest_pay_so_far + interest_paid
 
         mortgate_month_detail = {
             'payment': monthly_payment,
-            'interest': interest,
+            'interest_paid': interest_paid,
             'principal_paid': principal_paid,
             'remaining_principal': remain_principal,
             'interest_pay_so_far': interest_pay_so_far,
         }
 
         mortgate_details.append(mortgate_month_detail)
-        month_detail_list[i] = mortgate_month_detail
+        month_detail_list[the_nth_month] = mortgate_month_detail
+
+        number_of_payment = number_of_payment - 1
+        the_nth_month = the_nth_month + 1
 
     return mortgate_details
 
 
-def sum_interest(start, end):
+def range_interest_sum(start, end):
     """
     start: start at month
     end: end at month
@@ -71,23 +82,138 @@ def sum_interest(start, end):
     print("Total interest of from month {} to month {} is ${}".format(start, end, round(sum, 2)))
 
 
+def net_operating_income(liability: dict, income: int):
+    """
+    :params income: monthly income
+    :params liability:
+    """
+
+    assert 'monthly_mortgage_payment' in liability
+    assert 'property_tax' in liability
+    assert 'insurance' in liability
+
+    yearly_income = income
+
+    total_expense = 0
+    total_expense += liability['monthly_mortgage_payment']
+    total_expense += liability['hoa']
+    total_expense += liability['property_tax'] / 12
+    total_expense += liability['insurance'] / 12
+
+    return yearly_income - total_expense
+#
+# def table_output(content: list[dict]):
+#     if len(content) == 0:
+#         return
+#
+#     n_col = len(content[0].keys())
+#
+#     row_format = "{:>15}" * (n_col + 1)
+#     print(row_format.format("", *teams_list))
+#     for team, row in zip(teams_list, data):
+#         print(row_format.format(team, *row))
+
+def table_output(content_list, title_list):
+    n_col = len(title_list)
+    row_format = "{:>15}" * n_col
+    print(row_format.format(*title_list))
+    for c in content_list:
+        print(row_format.format(*c))
+
+def cal_income(house_info, loan_info, extra_principal_paid=None, month=25):
+    if extra_principal_paid is None:
+        extra_principal_paid = {}
+
+    price = house_info['price']
+    property_tax = house_info['property_tax']
+    insurance = house_info['insurance']
+    monthly_income = house_info['monthly_income']
+    hoa = house_info['hoa']
+
+    down_payment_type = loan_info['down_payment_type']
+    interest_rate = loan_info['interest_rate']
+    down_payment_percent = loan_info['down_payment_percent']
+    down_payment_amount = loan_info['down_payment_amout']
+    loan_time = loan_info['loan_time']
+
+    if down_payment_type == 'percent':
+        down_payment_amount = price * (1 - down_payment_percent / 0.01)
+
+    mortgate_details = mortgate_calculater(
+        price, down_payment_amount, interest_rate, loan_time,
+        is_year=True, extra_principal_paid=extra_principal_paid)
+
+    accumulated_NOI = 0
+    res_list = []
+    yearly_NOI = 0
+    for i in range(1, month):
+
+        # print(f'month: {i}')
+        md = mortgate_details[i]
+        monthly_payment = md.get('payment')
+
+        liability = dict()
+        liability['monthly_mortgage_payment'] = monthly_payment
+        liability['property_tax'] = property_tax / 12
+        liability['insurance'] = insurance / 12
+        liability['hoa'] = 0
+
+        monthly_NOI = round(net_operating_income(liability, monthly_income), 2)
+        accumulated_NOI = round(accumulated_NOI + monthly_NOI, 2)
+
+        # print(f'monthly_NOI: {monthly_NOI}')
+        # print(f'accumulated_NOI: {accumulated_NOI}')
+        if i % 12 == 0:
+            print(accumulated_NOI - yearly_NOI)
+            yearly_NOI = accumulated_NOI
+
+        res_element = [i, monthly_NOI, accumulated_NOI]
+        res_list.append(res_element)
+
+    return res_list
+
+
 def main():
-    # a = 22250,
 
-    # y = 0.09
-    # r = y/ 12
+    house_info = {
+        "price": 1150000.0,
+        "property_tax": 9964,
+        "insurance": 2400,
+        "monthly_income":  5500,
+        "hoa": 0,
+    }
 
-    # n = 36
+    loan_info = {
+        "interest_rate": 5.0,
+        "down_payment_amout": 100000,
+        "loan_time": 30,
+        "down_payment_type": 'amount',
+        "down_payment_percent": 0,
+    }
 
-    # interest_range = 60
+    extra_principal_paid = {
+        3: 50000,
+        4: 50000,
+        5: 50000,
+        6: 50000,
+        9: 50000,
+        10: 50000,
+        11: 50000,
+    }
 
+    res_list = cal_income(
+        house_info,
+        loan_info,
+        extra_principal_paid=extra_principal_paid,
+        month=49)
 
-    mortgate_details = mortgate_calculater(600000, 0.05, 30, is_year=True)
+    col_list = [
+        'month',
+        'monthly NOI',
+        'acc NOI',
+    ]
 
-    for i in range(10):
-        print(f'month: {i + 1}')
-        pprint.pprint(mortgate_details[i])
-
+    table_output(res_list, col_list)
 
 if __name__ == "__main__":
     main()
